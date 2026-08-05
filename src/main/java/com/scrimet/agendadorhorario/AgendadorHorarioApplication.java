5742c4f0115d6316a0e5f03d5b10702cbbcf2a3a
Lucasdelacerda <lacerdalucas27@gmail.com>
2026-08-05 01:20:22 -0300

    :tada:initial commit

---
+++ src/main/java/com/scrimet/agendadorhorario/AgendadorHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.scrimet.agendadorhorario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorHorarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendadorHorarioApplication.class, args);
+    }
+
+}
